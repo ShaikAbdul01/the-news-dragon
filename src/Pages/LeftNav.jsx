ba122b37640e496a26c3a0bd385f6eb5dcb5d486
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-04-29 14:17:52 +0600

    done

--- src/Pages/LeftNav.jsx
+++ src/Pages/LeftNav.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const LeftNav = () => {
   const [categories, setCategories] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/categories")
+    fetch("https://the-news-dragon-server-shaikabdul01.vercel.app/categories")
       .then((res) => res.json())
       .then((data) => setCategories(data))
       .catch((error) => console.error(error.message));
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -40,7 +40,7 @@ const router = createBrowserRouter([
         path: ":id",
         element: <Category></Category>,
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/categories/${params.id}`),
+          fetch(`https://the-news-dragon-server-shaikabdul01.vercel.app/categories/${params.id}`),
       },
     ],
   },
@@ -56,7 +56,7 @@ const router = createBrowserRouter([
           </PrivateRoute>
         ),
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/news/${params.id}`),
+          fetch(`https://the-news-dragon-server-shaikabdul01.vercel.app/news/${params.id}`),
       },
     ],
   },
